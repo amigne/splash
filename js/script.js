bfa2e53fccc851c48a7bc835052b054547f57c96
Yann Gauteron <yann@gauteron.me>
2018-03-20 20:36:01 +0100

    Modularizing SE (Search Engine)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
+
+
+  // This code will be called when the page has fully loaded
+  function init() {
+    window.se.init();
+//    init_se();
+  }
+
+
+
+  // MAIN
+  init();
+});
+
+///////////////////////////////////////////////////////////////////////////////
+
+// SE (Search Engine) logic
+(function(global) {
+  var se = {}
+
+  var current = null; // Current search engine in use
+
   // se_engines is a list of all predefined and supported search engines.
   // TODO: Obtain this list from the server!
-  var se_engines = {
+  var engines = {
     "bing": {
       "name": "Bing",
       "icon": "images/icon_bing_16.png",
@@ -28,103 +50,82 @@ $(document).ready(function () {
       "url": "https://search.yahoo.com/search?p="
     },
   };
-  var default_se = "google";
-  var current_se = null;
-  var old_se     = null;
+  var initial = "google";
 
-  // This code will be called when the page has fully loaded
-  function init() {
-    init_se();
-  }
-
-  // init_se() initializes the Search Engine form
-  function init_se() {
-    var dropdown = $("#se-dropdown");
-    var i; // Used for counting
-    var key; // Used for looping in tables
-    var html = ""; // Code to be inserted
-    var new_se = null;
-
-    // Initializes the event handlers when the enter key is pressed in the
-    // input field or the Go button is clicked
-    $("#se-in-query").keypress(function(event) { if ((event.keyCode || event.which) == '13') submitSearchEngineForm(); });
-    $("#se-btn-go").click(function() { submitSearchEngineForm(); });
-
-    // Make sure the default value exists, otherwise we'll use the first
+  se.init = function() {
+    // 1. Make sure the default value is valid, otherwise we'll use the first
     // search engine.
-    if (!se_engines[default_se] || !se_engines[default_se]["name"] || 
-        !se_engines[default_se]["icon"] || !se_engines[default_se]["url"]) {
-      default_se = null;
+    if (!engines[initial] || !engines[initial]["name"] || 
+        !engines[initial]["icon"] || !engines[initial]["url"]) {
+      initial = null;
     }
 
-    // Initializes the list of supported search engines in the dropdown
-    i = 0;
-    for (key in se_engines) {
-      var name = se_engines[key]["name"];
-      var icon = se_engines[key]["icon"];
-      var url = se_engines[key]["url"];
+    // 2. Initializes the event handlers when the enter key is pressed in the
+    // input field or the Go button is clicked
+    $("#se-in-query").keypress(function(event) { if ((event.keyCode || event.which) == '13') submitForm(); });
+    $("#se-btn-go").click(function() { submitForm(); });
 
-      html += '<a id="se-itm-' + key + '" class="dropdown-item';
-      if (default_se === key || (default_se === null && i === 0)) {
-        // html += ' font-weight-bold'; // Active engine is highlighted in bold
-        new_se = key;
-      }
-      html += '" href="#">';
+    // 3. Fill-in the drop-down table with the search engines
+    var i = 0;
+    var html = '';
+    for (var key in engines) {
+      var name = engines[key]['name'];
+      var icon = engines[key]['icon'];
+
+      html += '<a id="se-itm-' + key + '" class="dropdown-item" href="#">';
       html += '<img src="' + icon + '" class="pr-3"> ' + name;
-      html += '</a>' + "\n";
+      html += "</a>\n";
+
+      if (initial === null) {
+        // If we have no valid default, we reach this condition at
+        // the first loop. Let's set the default value.
+        initial = key;
+      }
     }
-    dropdown.html(html);
+    $("#se-dropdown").html(html);
 
-    // Initializes the event handler for the new objects
-    // MUST BE performed after having updated the DOM!
-    for (key in se_engines) {
-      $("#se-itm-" + key).click(function(event){ seChangeEngine(event['currentTarget']['id'].slice(7)); });
+    // 4. Initializes the event handler for the new objects
+    // MUST be performed after having updated the DOM!
+    for (key in engines) {
+      $("#se-itm-" + key).click(function(event){ changeEngine(event['currentTarget']['id'].slice(7)); });
     }
 
-    // Sets the icon for the default search engine
-    seChangeEngine(new_se);
-  }
+    // 5. Finally sets the GUI for the default search engine
+    changeEngine(initial);
+  };
 
-  function seChangeEngine(engine) {
-    // console.log("seChangeEngine(\"" + engine + "\")");
-    var img = $("#se-img-engine");
-    var item = null;
-
-    var icon = se_engines[engine]["icon"];
-    var name = se_engines[engine]["name"];
+  var changeEngine = function(engine) {
+    var classes;
 
     // 1. Update the icon
-    $("#se-img-engine").attr("src", icon);
+    $("#se-img-engine").attr("src", engines[engine]['icon']);
 
     // 2. Update the input search placeholder
-    $("#se-in-query").attr("placeholder", name);
+    // TODO: Use a sentence with I18N
+    $("#se-in-query").attr("placeholder", engines[engine]['name']);
 
     // 3. Unactivate the old SE in the list
-    if (current_se) {
-      item = $("#se-itm-" + current_se);
-      var classes = item.attr('class').split(/\s+/);
-      var newClasses = "";
-      for (var key in classes) {
-        if (classes[key] !== "font-weight-bold") {
-          newClasses += (newClasses ? " " : "") + classes[key];
-        }
-      }
-      item.attr('class', newClasses);
+    if (current) {
+      classes = $("#se-itm-" + current).attr('class');
+      classes = classes.replace(new RegExp('font-weight-bold'), ''); // deactivated: not bold anymore
+      classes = classes.replace(/\s{2,}/g, ' '); // clean-up: let's remove multiple spaces
+      classes = classes.replace(/^\s+|\s{2,}|\s+$/g, ''); // clean-up: remove leading+trailing spaces
+      $("#se-itm-" + current).attr('class', classes);
     }
 
     // 4. Activate the new SE in the list
-    item = $("#se-itm-" + engine);
-    classes = item.attr('class');
+    classes = $("#se-itm-" + engine).attr('class');
     if (classes.search("font-weight-bold") === -1) {
       classes += (classes ? " " : "") + "font-weight-bold";
     }
-    item.attr('class', classes);
+    $("#se-itm-" + engine).attr('class', classes);
 
-    current_se = engine;
-  }
+    // 5. Let's store the new engine as the current one
+    current = engine;
+  };
 
-  function submitSearchEngineForm() {
-    var value = $("#search-query").val();
+  function submitForm() {
+    var value = $("#se-in-query").val();
 
     if (!value) {
       // We don't have a valid entry, don't do anything!
@@ -133,12 +134,11 @@ $(document).ready(function () {
 
     value = encodeURIComponent(value);
 
-    var url = 'https://www.google.com/search?q=';
+    var url = engines[current]['url'];
     url += value;
 
     var win = window.open(url, "_blank");
   }
 
-  // MAIN
-  init();
-});
+  window.se = se;
+})(window);
